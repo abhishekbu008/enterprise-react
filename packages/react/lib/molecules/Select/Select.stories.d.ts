685b57e808b773993cbf472676bd044899d0ddbf
abhishekbu008 <abhishekbu30@gmail.com>
2021-09-10 21:49:28 +0530

    Fixed gitignore

---
+++ packages/react/lib/molecules/Select/Select.stories.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+import "@ds.e/scss/lib/Select.css";
+declare const _default: {
+    title: string;
+    decorators: import("@storybook/addons").DecoratorFunction<unknown>[];
+};
+export default _default;
+export declare const Common: () => JSX.Element;
+export declare const RenderOption: () => JSX.Element;
+export declare const CustomLabel: () => JSX.Element;
